a6b9a7f8614bface7795a8c702e5298c3b81144c
zombie1864 <zombie1864@gmail.com>
2021-09-28 15:42:12 -0400

    isolated modal css into its own css file

--- src/components/projects/ProjectModal.js
+++ src/components/projects/ProjectModal.js
@@ -1,30 +1,12 @@
 import React from 'react'
+import '../../css/projModal.css'
 
-const modalCss = {
-    position: 'fixed',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%,-50%)',
-    backgroundColor: '#FFF',
-    zIndex: 1, 
-    fontSize: '12px'
-}
-
-const overLayCss = {
-    position: 'fixed',
-    top: 0, 
-    left: 0,
-    right: 0, 
-    bottom: 0,
-    backgroundColor: 'rgba(0, 0, 0, 0.7)',
-    zIndex: 1 
-}
 
 const ProjectModal = ({openModal, projTitle, projPurpose, projTech, projAoA, ProjSrcCode, closeModal}) => { 
     if (!openModal) return null 
     return (
-        <div style={overLayCss}>
-        <div style={modalCss}>
+        <div className="modalOverlay">
+        <div className="modal">
             {projTitle}
             {projPurpose}
             {projTech}
