454fa1eef3c4d79a27e35dee293dab6561805bd6
zombie1864 <zombie1864@gmail.com>
2021-09-29 14:45:50 -0400

    btns now have animation upon loading page

--- src/components/projects/ProjectModal.js
+++ src/components/projects/ProjectModal.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import '../../css/projModal.css'
+import {motion} from 'framer-motion'
 
 
 const ProjectModal = ({openModal, projTitle, projPurpose, projTech, projAoA, ProjSrcCode, closeModal}) => { 
     if (!openModal) return null 
     return (
-        <div className="modalOverlay">
-        <div className="modal">
+        <motion.div 
+        className="modalBackdrop"
+        initial={{opacity: 0}} 
+        animate={{opacity: 1}} 
+        transition={{duration: 0.5}}>
+        <motion.div 
+        className="modal"
+        initial={{scale: 0}} 
+        animate={modalAnimation}>
             {projTitle}
             {projPurpose}
             {projTech}
             {projAoA}
             {ProjSrcCode}
             <button onClick={closeModal}>Close Modal</button>
-        </div>
-        </div>
+        </motion.div>
+        </motion.div>
     )
 }
 
 export default ProjectModal
+
+
+const modalAnimation = {
+    scale: 1,
+    x: -100,
+    y: 0,
+}
