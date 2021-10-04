7c7e774495bcc947e47c175f2f59449e1e1d2a61
zombie1864 <zombie1864@gmail.com>
2021-10-04 14:52:41 -0400

    updated carousel - now using my own - staging work for nxt phase, an indicator showing user where in the carousel they are in

--- src/components/Home/Carousel.js
+++ src/components/Home/Carousel.js
@@ -1,16 +1,48 @@
-import {Carousel as ThirdPartyCarousal} from '3d-react-carousal'
 import {imgSlides} from '../../utils/slides'
 import '../../css/carousal.css'
+import React, { useState } from 'react'
+import { CgChevronDoubleLeftR, CgChevronDoubleRightR} from 'react-icons/cg'
 
 const Carousel = () => {
     /**
     @description: This comp contains the carousal that will be rendered showing images/code snippets of python projects. To accomplish this npm i 3d-react-carousal was used. 
     **/
+    const [currImg, setCurrImg] = useState(0)
+    const carousalLength = imgSlides.length 
+    const nxtSlide = () => {
+        setCurrImg(currImg === carousalLength - 1 ? 0 : currImg + 1)
+    }
+    const prevSlide = () => {
+        setCurrImg(currImg === 0 ? carousalLength - 1 : currImg - 1)
+    }
+
+    
+   if (!Array.isArray(imgSlides) || imgSlides.length === -1 ) {
+       return null 
+   } // this piece of logic is for when there is no data slides or when imgSlides isn't an arr 
+   // this is only here for future scalability or additional features 
+
    return (
         <div className="carousalContainer">
-            {console.log('render')}
-            <ThirdPartyCarousal slides={imgSlides}/>
+            {/* <ThirdPartyCarousal slides={imgSlides}/> */}
+            <CgChevronDoubleLeftR className="leftArrow" onClick={prevSlide}/>
+            {imgSlides.map((imgObj, idx) => {
+                return (
+                    <div className={idx === currImg ? 'activeSlide' : 'slide'} key={idx}>
+                        {
+                            idx === currImg && (
+                                <img 
+                                src={imgObj.imgSrc} 
+                                alt={imgObj.alt} 
+                                className="carousalImg"/>
+                            )
+                        }
+                    </div>
+                )
+            })}
+            <CgChevronDoubleRightR className="rightArrow" onClick={nxtSlide}/>
         </div>
     )
 }
+
 export default Carousel
